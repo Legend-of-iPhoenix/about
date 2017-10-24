c0a7d66dfdaf4cbc806cafdf403e760d071fe50b
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-24 19:20:05 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+
 window.setInterval(function() {
-  alert('test');
   var random1 = 50 + Math.floor(Math.random() * 50);
   var random2 = 50 + Math.floor(Math.random() * 50);
   
   var button = document.querySelector('#button');
-  button.style.setProperty('--width-random', random1 +'px');
-  button.style.setProperty('--height-random', random2 +'px');
+  button.style.setProperty('--width-random', random1 +"px");
+  button.style.setProperty('--height-random', random2 +"px");
 } , 1000);
