65653bccceb99309a9a28ef390e34c909b079184
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-24 19:15:29 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+  var random1 = 50 + Math.floor(Math.random() * 50);
+  var random2 = 50 + Math.floor(Math.random() * 50);
+  
+  var button = document.querySelector('#button');
+  button.style.setProperty('--width-random', random1 +'px');
+  button.style.setProperty('--height-random', random2 +'px');
+} , 1000);
