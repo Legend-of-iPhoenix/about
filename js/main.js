5510d3551f8b6daf3a427e5f46ebc63bc5ad89d5
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-25 17:12:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-function randEverySec() {
-    window.setInterval(function() {
-      var random1 = 50 + Math.floor(Math.random() * 50);
-      var random2 = 50 + Math.floor(Math.random() * 50);
-  
-      var button = document.getElementByID("button");
-      button.style.width = random1; //+ "px";
-      button.style.height = random1; //+ "px";
+var id = null;
+
+//Thanks, stack overflow, for providing me with a much faster version of what I was going to do.
+function clamp(num, min, max) {
+  return num <= min ? min : num >= max ? max : num;
+}
+
+function randEverySec(button) {
+    id = window.setInterval(function() {
+      var random1 = 10 * (5 - Math.floor(Math.random() * 1));
+      var random2 = 10 * (5 - Math.floor(Math.random() * 10));
+      
+      
+      //the following works, but it doesn't 
+      button.style.width = clamp(button.style.width + random1, 20, 100) + "px";
+      button.style.height = clamp(button.style.height + random2, 20, 100) + "px";
+      
    } , 500);
+  }
+function killRandChange(button) {
+  window.clearInterval(id);
+  button.style.width = 20 +"px"; 
+  button.style.height = 20 +"px";
+}
+
+function toggleDiv(id) {
+    var div = document.getElementById(id);
+    div.style.display = div.style.display == "none" ? "block" : "none";
 }
