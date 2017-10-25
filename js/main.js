25a1477329b9337f477fe27bcc61b0911dede2c1
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-25 15:18:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function randEverySec() {
       var random1 = 50 + Math.floor(Math.random() * 50);
       var random2 = 50 + Math.floor(Math.random() * 50);
   
-      var button = document.querySelector('#button');
-      button.style.width = random1 + "px";
-      button.style.height = random1 + "px";
+      var button = document.getElementByID("button");
+      button.style.width = random1; //+ "px";
+      button.style.height = random1; //+ "px";
    } , 500);
 }
